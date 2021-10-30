55878b69a263b5f4a93f505464078c389bff6fc5
Andres Rodriguez <katsuro03@gmail.com>
2021-10-30 13:27:21 -0500

    migrates base de datos completas

---
+++ database/migrations/2021_10_30_180708_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->text('description');
+            $table->integer('minimum');
+            $table->double('purchase_value');
+            $table->double('sale_value');
+            $table->string('presentation');
+            $table->integer('total');
+
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->foreignId('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
