7c3498edefb6dd3a238ec3263d979a0940474f0d
Andres Rodriguez <katsuro03@gmail.com>
2021-10-30 09:41:21 -0500

    cambios por inicio de sesion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,29 +11,29 @@
                             <p class="text-muted">Sign In to your account</p>
                             <form action="{{route('login')}}" method="post">
                                 @csrf
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend"><span class="input-group-text">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend"><span class="input-group-text">
 <svg class="c-icon">
 <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
 </svg></span></div>
 
-                                <input name="Username" class="form-control" type="text" placeholder="Username">
-                            </div>
-                            <div class="input-group mb-4">
-                                <div class="input-group-prepend"><span class="input-group-text">
+                                    <input name="Username" class="form-control" type="text" placeholder="Username">
+                                </div>
+                                <div class="input-group mb-4">
+                                    <div class="input-group-prepend"><span class="input-group-text">
 <svg class="c-icon">
 <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
 </svg></span></div>
-                                <input class="form-control" name="Password" type="password" placeholder="Password">
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <button class="btn btn-primary px-4" type="submit">Login</button>
+                                    <input class="form-control" name="Password" type="password" placeholder="Password">
                                 </div>
-                                <div class="col-6 text-right">
-                                    <button class="btn btn-link px-0" type="button">Forgot password?</button>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <button class="btn btn-primary px-4" type="submit">Login</button>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <button class="btn btn-link px-0" type="button">Forgot password?</button>
+                                    </div>
                                 </div>
-                            </div>
                         </div>
                     </div>
                     </form>
@@ -41,7 +41,8 @@
                         <div class="card-body text-center">
                             <div>
                                 <h2>Sign up</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.</p>
                                 <button class="btn btn-lg btn-outline-light mt-3" type="button">Register Now!</button>
                             </div>
                         </div>
